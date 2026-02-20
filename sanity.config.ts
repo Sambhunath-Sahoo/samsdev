7b33776aa1b99d91b6c17ed4023be0facc90801d
Sambhu <sambhunath@recruitcrm.io>
2026-02-20 23:36:43 +0530

    Update Sanity configuration to support multiple environment variables: Modify projectId and dataset in sanity.config.ts and client.ts to use NEXT_PUBLIC environment variables as fallback. Update index.html to reflect new sanity CDN URLs and remove obsolete sanity-CuTT8LHf.js file. Adjust create-manifest.json timestamp for consistency.

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   name: "default",
   title: "samsdev",
 
-  projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
-  dataset: process.env.SANITY_STUDIO_DATASET!,
+  projectId: (process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || process.env.SANITY_STUDIO_PROJECT_ID)!,
+  dataset: (process.env.NEXT_PUBLIC_SANITY_DATASET || process.env.SANITY_STUDIO_DATASET)!,
 
   plugins: [structureTool(), visionTool()],
 
